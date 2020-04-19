13503c8c748a1962b81d4f42f08b57a568a48839
max13fr <max13fr@yozo.fr>
2020-04-19 17:54:21 +0200

    Bump version de 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="AirzoneCloud",
-    version="0.0.1",
+    version="0.1.0",
     author="max13fr",
     author_email="max13fr@yozo.fr",
     description="Python3 library for Airzone Cloud API",
@@ -16,7 +16,7 @@ setuptools.setup(
     packages=["AirzoneCloud"],
     install_requires=["requests"],
     classifiers=[
-        "Development Status :: 3 - Alpha",  # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        "Development Status :: 4 - Beta",  # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
